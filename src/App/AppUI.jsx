4a8aca38b6650d485883ef503d485dee592a0804
Marianatprograma <maritaborue@gmail.com>
2022-03-10 07:24:37 -0500

    useContext

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -1,33 +1,32 @@
 import React from "react";
+import { TodoContext } from "../TodoContext";
 import { TodoCounter } from "../TodoCounter";
 import { TodoSearch } from "../TodoSearch";
 import { TodoList } from "../TodoList";
 import { TodoItem } from "../TodoItem";
 import { CreateTodoButton } from "../CreateTodoButton";
 
-function AppUI({
-  loading,
-  error,
-  totalTodos,
-  completedTodos,
-  searchValue,
-  setSearchValue,
-  searchedTodos,
-  toggleCompleteTodo,
-  deleteTodo,
-}) {
-  return (
-    <>
-      <TodoCounter total={totalTodos} completed={completedTodos} />
+function AppUI() {
+  const { 
+    error,
+     loading, 
+     searchedTodos, 
+     toggleCompleteTodo, 
+     deleteTodo
+    } = React.useContext(TodoContext);
 
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+  return (
+    
+    <>
+      <TodoCounter />
+
+      <TodoSearch />
 
       <TodoList>
+        {error && <p>Hubo un error, desesperate </p>}
+        {loading && <p>Estamos cargando, no desesperes </p>}
 
-        {error && <p>Hubo un error, desesperate </p> }
-        {loading && <p>Estamos cargando, no desesperes </p> }
-
-        {(!loading && !searchedTodos.length) && <p> Crea tu primer TODO </p> }
+        {!loading && !searchedTodos.length && <p> Crea tu primer TODO </p>}
 
         {searchedTodos.map((todo) => (
           <TodoItem
@@ -39,6 +38,7 @@ function AppUI({
           />
         ))}
       </TodoList>
+
       <CreateTodoButton />
     </>
   );
